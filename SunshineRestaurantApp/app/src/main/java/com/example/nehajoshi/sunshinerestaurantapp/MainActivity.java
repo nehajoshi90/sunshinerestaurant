d56aec454a1217fc2e4c3c4c34b746627fd9eb78
Neha Joshi <nehaa.90@gmail.com>
2018-07-05 09:03:48 +0530

    changed ui to display restaurant and weather option if user location is successfully received

--- SunshineRestaurantApp/app/src/main/java/com/example/nehajoshi/sunshinerestaurantapp/MainActivity.java
+++ SunshineRestaurantApp/app/src/main/java/com/example/nehajoshi/sunshinerestaurantapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.nehajoshi.sunshinerestaurantapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -8,9 +9,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.nehajoshi.sunshinerestaurantapp.restaturant.RestaurantActivity;
 import com.example.nehajoshi.sunshinerestaurantapp.utils.Constants;
+import com.example.nehajoshi.sunshinerestaurantapp.weather.WeatherActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -56,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     // permission denied, boo! Disable the
                     // functionality that depends on this permission.
-                    statusTxtv.setText("Permission not granted. Unable to proceed further!");
+                    statusTxtv.setText(getString(R.string.permission_not_granted));
                 }
                 return;
             }
@@ -72,11 +76,20 @@ public class MainActivity extends AppCompatActivity {
                         // Got last known location. In some rare situations this can be null.
                         if (location != null) {
                             // Logic to handle location object
-                            statusTxtv.setText("Device location : " + location.getLatitude());
+                            statusTxtv.setVisibility(View.GONE);
+                            findViewById(R.id.layout).setVisibility(View.VISIBLE);
                         }else{
-                            statusTxtv.setText("Unable to get user location");
+                            statusTxtv.setText(getString(R.string.unable_to_get_location));
                         }
                     }
                 });
+    }
+
+    public void viewWeatherForecast(View view){
+        startActivity(new Intent(this, WeatherActivity.class));
+    }
+
+    public void viewNearrestra(View view){
+        startActivity(new Intent(this, RestaurantActivity.class));
     }
 }
---
+++ SunshineRestaurantApp/app/src/main/java/com/example/nehajoshi/sunshinerestaurantapp/weather/WeatherActivity.java
@@ -0,0 +1,14 @@
+package com.example.nehajoshi.sunshinerestaurantapp.weather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.nehajoshi.sunshinerestaurantapp.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
